cf0f127985e9226fe2d1a665d881e0c5f2e19c80
Christopher Bell <cb.camellia@gmail.com>
2019-07-16 21:50:31 -0400

    Apply gitignore rules

--- Firmware/build/default/production/main.o.d
+++
@@ -1,1 +0,0 @@
-C:\Users\chrsb\MPLABXProjects\ps2mini.X\build\default\production\main.o: main.c device.h 33CK64MP105.h configuration.h ps2_protocol.h 
--- Firmware/build/default/production/newmain.o.d
+++
@@ -1,1 +0,0 @@
-C:\Users\chrsb\MPLABXProjects\ps2mini.X\build\default\production\newmain.o: newmain.c 33CK64MP105.h stdio.h string.h stddef.h ctype.h stdlib.h 
